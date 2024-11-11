e26499bee03998464500d1b8707d3b6b3c831928
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-11 10:16:17 -0500

    this is actually the state of the art for tests

--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -1,9 +1,12 @@
 // See README.md for license details.
+
 package gcd
 
 import chisel3._
+import chisel3.experimental.BundleLiterals._
 import chisel3.simulator.EphemeralSimulator._
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
 
 /** This is a trivial example of how to run this Specification From within sbt
   * use:
@@ -16,57 +19,57 @@ import org.scalatest.flatspec.AnyFlatSpec
   * }}}
   * Testing from mill:
   * {{{
-  * mill NeuralNetworkFPGA.test.testOnly gcd.GCDSpec
+  * mill %NAME%.test.testOnly gcd.GCDSpec
   * }}}
   */
-class GCDSpec extends AnyFlatSpec {
+class GCDSpec extends AnyFreeSpec with Matchers {
 
-  behavior of "GCD"
+  "Gcd should calculate proper greatest common denominator" in {
+    simulate(new DecoupledGcd(16)) { dut =>
+      val testValues = for { x <- 0 to 10; y <- 0 to 10 } yield (x, y)
+      val inputSeq = testValues.map { case (x, y) =>
+        (new GcdInputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U)
+      }
+      val resultSeq = testValues.map { case (x, y) =>
+        (new GcdOutputBundle(16)).Lit(
+          _.value1 -> x.U,
+          _.value2 -> y.U,
+          _.gcd -> BigInt(x).gcd(BigInt(y)).U
+        )
+      }
 
-  it should "calculate the GCD of two numbers" in {
-    simulate(new GCD) { c =>
-      // Test case 1: GCD(20, 12)
-      c.io.value1.poke(20.U)
-      c.io.value2.poke(12.U)
-      c.io.loadingValues.poke(true.B)
-      c.clock.step()
-      c.io.outputValid.expect(true.B)
-      // Check if the GCD is calculated correctly (GCD(20, 12) should be 4)
-      println("GCD(20, 12): " + c.io.outputGCD.peek().litValue)
-      assert(c.io.outputGCD.peek().litValue == 4)
+      dut.reset.poke(true.B)
+      dut.clock.step()
+      dut.reset.poke(false.B)
+      dut.clock.step()
 
-      // Test case 2: GCD(42, 56)
-      // c.io.value1.poke(42.U)
-      // c.io.value2.poke(56.U)
-      // c.io.loadingValues.poke(true.B)
-      // c.clock.step()
-      // c.io.loadingValues.poke(false.B)
-      // c.clock.step()
-      // // Check if the GCD is calculated correctly (GCD(42, 56) should be 14)
-      // println("GCD(42, 56): " + c.io.outputGCD.peek().litValue)
-      // assert(c.io.outputGCD.peek().litValue == 14)
+      var sent, received, cycles: Int = 0
+      while (sent != 100 && received != 100) {
+        assert(cycles <= 1000, "timeout reached")
 
-      // // Test case 3: GCD(7, 13), prime numbers
-      // c.io.value1.poke(7.U)
-      // c.io.value2.poke(13.U)
-      // c.io.loadingValues.poke(true.B)
-      // c.clock.step()
-      // c.io.loadingValues.poke(false.B)
-      // c.clock.step()
-      // // Check if the GCD is calculated correctly (GCD(7, 13) should be 1)
-      // println("GCD(7, 13): " + c.io.outputGCD.peek().litValue)
-      // assert(c.io.outputGCD.peek().litValue == 1)
+        if (sent < 100) {
+          dut.input.valid.poke(true.B)
+          dut.input.bits.value1.poke(testValues(sent)._1.U)
+          dut.input.bits.value2.poke(testValues(sent)._2.U)
+          if (dut.input.ready.peek().litToBoolean) {
+            sent += 1
+          }
+        }
 
-      // // Test case 4: GCD(100, 100)
-      // c.io.value1.poke(100.U)
-      // c.io.value2.poke(100.U)
-      // c.io.loadingValues.poke(true.B)
-      // c.clock.step()
-      // c.io.loadingValues.poke(false.B)
-      // c.clock.step()
-      // // Check if the GCD is calculated correctly (GCD(100, 100) should be 100)
-      // println("GCD(100, 100): " + c.io.outputGCD.peek().litValue)
-      // assert(c.io.outputGCD.peek().litValue == 100)
+        if (received < 100) {
+          dut.output.ready.poke(true.B)
+          if (dut.output.valid.peekValue().asBigInt == 1) {
+            dut.output.bits.gcd.expect(
+              BigInt(testValues(received)._1).gcd(testValues(received)._2)
+            )
+            received += 1
+          }
+        }
+
+        // Step the simulation forward.
+        dut.clock.step()
+        cycles += 1
+      }
     }
   }
 }
