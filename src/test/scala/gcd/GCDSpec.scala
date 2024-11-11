7793ca7adb0056c8e421a8067a777b21926b2033
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-10 22:51:40 -0500

    updated the scala test using the state of the art@

--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -1,12 +1,9 @@
 // See README.md for license details.
-
 package gcd
 
 import chisel3._
-import chisel3.experimental.BundleLiterals._
 import chisel3.simulator.EphemeralSimulator._
-import org.scalatest.freespec.AnyFreeSpec
-import org.scalatest.matchers.must.Matchers
+import org.scalatest.flatspec.AnyFlatSpec
 
 /**
   * This is a trivial example of how to run this Specification
@@ -23,46 +20,55 @@ import org.scalatest.matchers.must.Matchers
   * mill NeuralNetworkFPGA.test.testOnly gcd.GCDSpec
   * }}}
   */
-class GCDSpec extends AnyFreeSpec with Matchers {
+class GCDSpec extends AnyFlatSpec {
 
-  "Gcd should calculate proper greatest common denominator" in {
-    simulate(new DecoupledGcd(16)) { dut =>
-      val testValues = for { x <- 0 to 10; y <- 0 to 10} yield (x, y)
-      val inputSeq = testValues.map { case (x, y) => (new GcdInputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U) }
-      val resultSeq = testValues.map { case (x, y) =>
-        (new GcdOutputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U, _.gcd -> BigInt(x).gcd(BigInt(y)).U)
-      }
+  behavior of "GCD"
 
-      dut.reset.poke(true.B)
-      dut.clock.step()
-      dut.reset.poke(false.B)
-      dut.clock.step()
+  it should "calculate the GCD of two numbers" in {
+    simulate(new GCD) { c =>
+      // Test case 1: GCD(20, 12)
+      c.io.value1.poke(20.U)
+      c.io.value2.poke(12.U)
+      c.io.loadingValues.poke(true.B) // Set loadingValues to true to load inputs
+      c.clock.step() // Step the clock once to load values into the registers
+      c.io.loadingValues.poke(false.B) // Set loadingValues to false after loading inputs
+      c.clock.step() // Step the clock again to compute the GCD
+      // Check if the GCD is calculated correctly (GCD(20, 12) should be 4)
+      println("GCD(20, 12): " + c.io.outputGCD.peek().litValue)
+      assert(c.io.outputGCD.peek().litValue == 4)
 
-      var sent, received, cycles: Int = 0
-      while (sent != 100 && received != 100) {
-        assert(cycles <= 1000, "timeout reached")
+      // Test case 2: GCD(42, 56)
+      c.io.value1.poke(42.U)
+      c.io.value2.poke(56.U)
+      c.io.loadingValues.poke(true.B)
+      c.clock.step()
+      c.io.loadingValues.poke(false.B)
+      c.clock.step()
+      // Check if the GCD is calculated correctly (GCD(42, 56) should be 14)
+      println("GCD(42, 56): " + c.io.outputGCD.peek().litValue)
+      assert(c.io.outputGCD.peek().litValue == 14)
 
-        if (sent < 100) {
-          dut.input.valid.poke(true.B)
-          dut.input.bits.value1.poke(testValues(sent)._1.U)
-          dut.input.bits.value2.poke(testValues(sent)._2.U)
-          if (dut.input.ready.peek().litToBoolean) {
-            sent += 1
-          }
-        }
+      // Test case 3: GCD(7, 13), prime numbers
+      c.io.value1.poke(7.U)
+      c.io.value2.poke(13.U)
+      c.io.loadingValues.poke(true.B)
+      c.clock.step()
+      c.io.loadingValues.poke(false.B)
+      c.clock.step()
+      // Check if the GCD is calculated correctly (GCD(7, 13) should be 1)
+      println("GCD(7, 13): " + c.io.outputGCD.peek().litValue)
+      assert(c.io.outputGCD.peek().litValue == 1)
 
-        if (received < 100) {
-          dut.output.ready.poke(true.B)
-          if (dut.output.valid.peekValue().asBigInt == 1) {
-            dut.output.bits.gcd.expect(BigInt(testValues(received)._1).gcd(testValues(received)._2))
-            received += 1
-          }
-        }
-
-        // Step the simulation forward.
-        dut.clock.step()
-        cycles += 1
-      }
+      // Test case 4: GCD(100, 100)
+      c.io.value1.poke(100.U)
+      c.io.value2.poke(100.U)
+      c.io.loadingValues.poke(true.B)
+      c.clock.step()
+      c.io.loadingValues.poke(false.B)
+      c.clock.step()
+      // Check if the GCD is calculated correctly (GCD(100, 100) should be 100)
+      println("GCD(100, 100): " + c.io.outputGCD.peek().litValue)
+      assert(c.io.outputGCD.peek().litValue == 100)
     }
   }
 }
