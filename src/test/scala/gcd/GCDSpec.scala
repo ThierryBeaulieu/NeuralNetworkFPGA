0d416913c46987dddaf51a3926fe674eebbb0f9c
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-11 11:11:16 -0500

    add test for basic GCD module

--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.must.Matchers
 
 class GCDSpec extends AnyFreeSpec with Matchers {
 
-  "Gcd should calculate proper greatest common divisor" in {
+  "Gcd decoupled should calculate proper greatest common divisor" in {
     simulate(new DecoupledGcd(16)) { dut =>
       val testCases = Seq(
         (8, 12, 4),
@@ -46,4 +46,45 @@ class GCDSpec extends AnyFreeSpec with Matchers {
       }
     }
   }
+
+  class GCDSpec extends AnyFreeSpec with Matchers {
+
+    "GCD module should compute the greatest common divisor" in {
+      simulate(new GCD) { dut =>
+        // Helper function to perform a single GCD computation
+        def computeGCD(a: Int, b: Int): Int = {
+          if (b == 0) a else computeGCD(b, a % b)
+        }
+
+        // Test cases with known GCD results
+        val testCases = Seq(
+          (27, 15), // GCD = 3
+          (21, 14), // GCD = 7
+          (32, 12), // GCD = 4
+          (9, 28), // GCD = 1
+          (100, 25) // GCD = 25
+        )
+
+        for ((a, b) <- testCases) {
+          val expectedGCD = computeGCD(a, b)
+
+          // Load the values into the DUT
+          dut.io.value1.poke(a.U)
+          dut.io.value2.poke(b.U)
+          dut.io.loadingValues.poke(true.B)
+          dut.clock.step(1)
+          dut.io.loadingValues.poke(false.B)
+
+          // Step through the calculation until output is valid
+          while (!dut.io.outputValid.peek().litToBoolean) {
+            dut.clock.step(1)
+          }
+
+          // Check that the output matches the expected GCD
+          dut.io.outputGCD.expect(expectedGCD.U)
+        }
+      }
+    }
+  }
+
 }
