60c7444cb7ab6d11334a413762b13e4bff2d116a
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-18 17:06:33 -0500

    BitwiseAND module tested exhaustively

--- src/test/scala/project/BitwiseANDSpec.scala
+++ src/test/scala/project/BitwiseANDSpec.scala
@@ -77,4 +77,47 @@ class BitwiseANDSpec extends AnyFreeSpec with Matchers {
       }
     }
   }
+
+  "Should produce return a stream of bipolar values with bit {0, 1}" in {
+    simulate(new BitwiseAND) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+      val inputIntegers = Seq(
+        1.S(2.W),
+        0.S(2.W),
+        -1.S(2.W),
+        1.S(2.W),
+        0.S(2.W),
+        -1.S(2.W)
+      )
+      val inputBit = Seq(
+        0.U(1.W),
+        0.U(1.W),
+        0.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W)
+      )
+      val expectedUnipolarStream =
+        Seq(
+          0.S(2.W),
+          0.S(2.W),
+          0.S(2.W),
+          1.S(2.W),
+          0.S(2.W),
+          -1.S(2.W)
+        )
+
+      for (i <- 0 until expectedUnipolarStream.length) {
+        dut.io.inputInteger.poke(inputIntegers(i))
+        dut.io.inputBit.poke(inputBit(i))
+        dut.clock.step(1)
+        dut.io.outputStream.expect(expectedUnipolarStream(i))
+      }
+    }
+  }
 }
