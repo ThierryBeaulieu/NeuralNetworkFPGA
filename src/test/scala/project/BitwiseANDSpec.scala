eb11eb9716942c5f5939aa28da91303284b46850
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-18 17:04:44 -0500

    BitwiseAND test for bit = 0 works

--- src/test/scala/project/BitwiseANDSpec.scala
+++ src/test/scala/project/BitwiseANDSpec.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.must.Matchers
 
 class BitwiseANDSpec extends AnyFreeSpec with Matchers {
 
-  "Should produce return a stream of bipolar values" in {
+  "Should produce return a stream of bipolar values with bit = 1" in {
     simulate(new BitwiseAND) { dut =>
       // Reset the DUT
       dut.reset.poke(true.B)
@@ -33,6 +33,40 @@ class BitwiseANDSpec extends AnyFreeSpec with Matchers {
           1.S(2.W),
           0.S(2.W),
           -1.S(2.W)
+        )
+
+      for (i <- 0 until expectedUnipolarStream.length) {
+        dut.io.inputInteger.poke(inputIntegers(i))
+        dut.io.inputBit.poke(inputBit(i))
+        dut.clock.step(1)
+        dut.io.outputStream.expect(expectedUnipolarStream(i))
+      }
+    }
+  }
+
+  "Should produce return a stream of bipolar values with bit = 0" in {
+    simulate(new BitwiseAND) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+      val inputIntegers = Seq(
+        1.S(2.W),
+        0.S(2.W),
+        -1.S(2.W)
+      )
+      val inputBit = Seq(
+        0.U(1.W),
+        0.U(1.W),
+        0.U(1.W)
+      )
+      val expectedUnipolarStream =
+        Seq(
+          0.S(2.W),
+          0.S(2.W),
+          0.S(2.W)
         )
 
       for (i <- 0 until expectedUnipolarStream.length) {
