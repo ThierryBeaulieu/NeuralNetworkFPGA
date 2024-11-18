afc10f7702156aaea5a6ee5e869b58b8786eeaaf
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-18 18:50:24 -0500

    TreeAdder add a verification so see if it's possible to add 6 values together and it's possible

--- src/test/scala/project/TreeAdderSpec.scala
+++ src/test/scala/project/TreeAdderSpec.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.must.Matchers
 
 class TreeAdderSpec extends AnyFreeSpec with Matchers {
 
-  "Should produce a bipolar stochastic stream between {-2, 2}" in {
+  "Should add 4 {1} bipolar stochastic stream to get 4" in {
     simulate(new TreeAdder(nbStream = 4)) { dut =>
       // Reset the DUT
       dut.reset.poke(true.B)
@@ -24,6 +24,48 @@ class TreeAdderSpec extends AnyFreeSpec with Matchers {
       dut.io.inputStream(1).poke(1.S)
       dut.io.inputStream(2).poke(1.S)
       dut.io.inputStream(3).poke(1.S)
+      dut.clock.step(1)
+
+      dut.io.outputStream.expect(expectedUnipolarStream)
+    }
+  }
+
+  "Should add 4 {-1} bipolar stochastic stream to get -4" in {
+    simulate(new TreeAdder(nbStream = 4)) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+      val expectedUnipolarStream = -4.S(3.W)
+      // incoming bipolar stochastic stream
+      dut.io.inputStream(0).poke(-1.S)
+      dut.io.inputStream(1).poke(-1.S)
+      dut.io.inputStream(2).poke(-1.S)
+      dut.io.inputStream(3).poke(-1.S)
+      dut.clock.step(1)
+
+      dut.io.outputStream.expect(expectedUnipolarStream)
+    }
+  }
+
+  "Should add 6 bipolar stochastic stream together" in {
+    simulate(new TreeAdder(nbStream = 6)) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+      val expectedUnipolarStream = -2.S(3.W)
+      // incoming bipolar stochastic stream
+      dut.io.inputStream(0).poke(-1.S)
+      dut.io.inputStream(1).poke(-1.S)
+      dut.io.inputStream(2).poke(1.S)
+      dut.io.inputStream(3).poke(-1.S)
+      dut.io.inputStream(4).poke(1.S)
+      dut.io.inputStream(5).poke(-1.S)
       dut.clock.step(1)
 
       dut.io.outputStream.expect(expectedUnipolarStream)
