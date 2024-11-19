9415bfd20d13c4a37e2a92b72cf32c720b2275d7
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-18 20:20:02 -0500

    Neuron. Generate multiple bipolar stream based on several weights

--- src/main/scala/project/Neuron.scala
+++ src/main/scala/project/Neuron.scala
@@ -9,13 +9,15 @@ import chisel3._
   * @param inputWeights
   *   weight with a value of [-128, 127]
   * @param outputB2SValues
-  *   bipolar stream {0, 1}
+  *   unipolar stream {0, 1}
+  * @param outputB2ISValues
+  *   bipolar stream {-1, 1}
   * @param outputStream
-  *   bipolar stream {0, 1}
+  *   unipolar stream {0, 1}
   */
 class Neuron(nbData: Int) extends Module {
   private val b2SUnipolar = Seq.fill(nbData)(Module(new B2SUnipolar))
-  // private val b2ISBipolar = Module(new B2ISBipolar())
+  private val b2ISBipolar = Seq.fill(nbData)(Module(new B2ISBipolar))
   // private val treeAdder = Module(new TreeAdder(nbStream = nbData))
   // private val nStanh = Module(new NStanh(offset = 2.S, mn = 6.S))
 
@@ -23,19 +25,26 @@ class Neuron(nbData: Int) extends Module {
     val inputPixels = Input(Vec(nbData, UInt(8.W)))
     val inputWeights = Input(Vec(nbData, SInt(8.W)))
     val outputB2SValues = Output(Vec(nbData, UInt(1.W)))
+    val outputB2ISValues = Output(Vec(nbData, SInt(2.W)))
     val outputStream = Output(Vec(nbData, UInt(1.W)))
   })
 
   // Step 1. Pixel Unipolar Conversion
-  val regStochastic = RegInit(VecInit(Seq.fill(nbData)(0.U(1.W))))
-  for (i <- 0 until regStochastic.length) {
+  val regB2S = RegInit(VecInit(Seq.fill(nbData)(0.U(1.W))))
+  for (i <- 0 until regB2S.length) {
     b2SUnipolar(i).io.inputPixel := io.inputPixels(i)
-    regStochastic(i) := b2SUnipolar(i).io.outputStream
-    io.outputB2SValues(i) := regStochastic(i)
-    io.outputStream(i) := regStochastic(i)
+    regB2S(i) := b2SUnipolar(i).io.outputStream
+    io.outputB2SValues(i) := regB2S(i)
+    io.outputStream(i) := regB2S(i) // todo assign last value at the end
   }
 
   // Step 2. Weight Bipolar Conversion
+  val regB2IS = RegInit(VecInit(Seq.fill(nbData)(0.S(2.W))))
+  for (i <- 0 until regB2IS.length) {
+    b2ISBipolar(i).io.inputWeight := io.inputWeights(i)
+    regB2IS(i) := b2ISBipolar(i).io.outputStream
+    io.outputB2ISValues(i) := regB2IS(i)
+  }
 
   // Step 3. Pixel & Weight
 
--- src/test/scala/project/NeuronSpec.scala
+++ src/test/scala/project/NeuronSpec.scala
@@ -102,4 +102,87 @@ class NeuronSpec extends AnyFreeSpec with Matchers {
       }
     }
   }
+
+  "Neuron should generate bipolar stream with multiple B2ISBipolar" in {
+    simulate(new Neuron(nbData = 3)) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+      // incoming bipolar stochastic stream
+      dut.io.inputPixels(0).poke(127.U)
+      dut.io.inputWeights(0).poke(0.S)
+
+      dut.io.inputPixels(1).poke(127.U)
+      dut.io.inputWeights(1).poke(127.S)
+
+      dut.io.inputPixels(2).poke(127.U)
+      dut.io.inputWeights(2).poke(-127.S)
+
+      val expectedBipolarStream1 = Seq(
+        1.S(2.W),
+        -1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        -1.S(2.W),
+        1.S(2.W),
+        -1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        -1.S(2.W),
+        1.S(2.W)
+      )
+
+      val expectedBipolarStream2 = Seq(
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W),
+        1.S(2.W)
+      )
+
+      val expectedBipolarStream3 = Seq(
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W),
+        -1.S(2.W)
+      )
+
+      for (i <- 0 until expectedBipolarStream1.length) {
+        dut.clock.step(1)
+        // print(dut.io.outputB2ISValues(0).peek().litValue)
+        dut.io.outputB2ISValues(0).expect(expectedBipolarStream1(i))
+      }
+
+      for (i <- 0 until expectedBipolarStream2.length) {
+        dut.clock.step(1)
+        // print(dut.io.outputB2ISValues(1).peek().litValue)
+        dut.io.outputB2ISValues(1).expect(expectedBipolarStream2(i))
+      }
+
+      for (i <- 0 until expectedBipolarStream3.length) {
+        dut.clock.step(1)
+        // print(dut.io.outputB2ISValues(2).peek().litValue)
+        dut.io.outputB2ISValues(2).expect(expectedBipolarStream3(i))
+      }
+    }
+  }
 }
