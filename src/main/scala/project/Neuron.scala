0b4d85d51adf6f6f65cdce168586de57993324ca
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-18 20:09:30 -0500

    Neuron. able to create multiple b2s according to the Neuron input

--- src/main/scala/project/Neuron.scala
+++ src/main/scala/project/Neuron.scala
@@ -14,7 +14,7 @@ import chisel3._
   *   bipolar stream {0, 1}
   */
 class Neuron(nbData: Int) extends Module {
-  private val b2SUnipolar = Module(new B2SUnipolar())
+  private val b2SUnipolar = Seq.fill(nbData)(Module(new B2SUnipolar))
   // private val b2ISBipolar = Module(new B2ISBipolar())
   // private val treeAdder = Module(new TreeAdder(nbStream = nbData))
   // private val nStanh = Module(new NStanh(offset = 2.S, mn = 6.S))
@@ -28,10 +28,12 @@ class Neuron(nbData: Int) extends Module {
 
   // Step 1. Pixel Unipolar Conversion
   val regStochastic = RegInit(VecInit(Seq.fill(nbData)(0.U(1.W))))
-  b2SUnipolar.io.inputPixel := io.inputPixels(0)
-  regStochastic(0) := b2SUnipolar.io.outputStream
-  io.outputB2SValues(0) := regStochastic(0)
-  io.outputStream(0) := regStochastic(0)
+  for (i <- 0 until regStochastic.length) {
+    b2SUnipolar(i).io.inputPixel := io.inputPixels(i)
+    regStochastic(i) := b2SUnipolar(i).io.outputStream
+    io.outputB2SValues(i) := regStochastic(i)
+    io.outputStream(i) := regStochastic(i)
+  }
 
   // Step 2. Weight Bipolar Conversion
 
--- src/test/scala/project/NeuronSpec.scala
+++ src/test/scala/project/NeuronSpec.scala
@@ -43,4 +43,63 @@ class NeuronSpec extends AnyFreeSpec with Matchers {
       }
     }
   }
+
+  "Neuron should generate stream with multiple B2S" in {
+    simulate(new Neuron(nbData = 2)) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+      // incoming bipolar stochastic stream
+      dut.io.inputPixels(0).poke(127.U)
+      dut.io.inputWeights(0).poke(127.S)
+
+      dut.io.inputPixels(1).poke(255.U)
+      dut.io.inputWeights(1).poke(127.S)
+
+      val expectedUnipolarStream1 = Seq(
+        1.U(1.W),
+        0.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        0.U(1.W),
+        1.U(1.W),
+        0.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        0.U(1.W),
+        1.U(1.W)
+      )
+
+      val expectedUnipolarStream2 = Seq(
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W),
+        1.U(1.W)
+      )
+
+      for (i <- 0 until expectedUnipolarStream1.length) {
+        dut.clock.step(1)
+        // print(dut.io.outputB2SValues(0).peek().litValue)
+        dut.io.outputB2SValues(0).expect(expectedUnipolarStream1(i))
+      }
+
+      for (i <- 0 until expectedUnipolarStream2.length) {
+        dut.clock.step(1)
+        // print(dut.io.outputB2SValues(1).peek().litValue)
+        dut.io.outputB2SValues(1).expect(expectedUnipolarStream2(i))
+      }
+    }
+  }
 }
