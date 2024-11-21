c8e60b9f0faa918f505e4c68a258decc430edae7
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-21 10:59:28 -0500

    the approximation works

--- src/lab3/main.py
+++ src/lab3/main.py
@@ -33,8 +33,8 @@ for i in range(0, len(image)):
 
 # Step 2. Converts Weights And Saves Them In CSV [2:6]
 # Hidden Layer 1 [-1.463356, 1.00899]
-weightsHidden1 = np.load("theta_0.npy").T # (401, 25)
-weightsHidden1Int8 = np.zeros((401, 26))
+weightsHidden1 = np.load("theta_0.npy") # (25, 401)
+weightsHidden1Int8 = np.zeros((25, 401))
 for i in range(0, len(weightsHidden1)):
     weightRow = weightsHidden1[i]
     for j in range(0, len(weightRow)):
@@ -44,7 +44,7 @@ for i in range(0, len(weightsHidden1)):
 np.savetxt('theta_0_int8.csv', weightsHidden1Int8, delimiter=',', fmt='%d')
 
 # Hidden Layer 2 [-4.0308, 3.2115848]
-weightsHidden2 = np.load("theta_1.npy").T # (26, 10)
+weightsHidden2 = np.load("theta_1.npy") # (10, 26)
 weightsHidden2Int8 = np.zeros_like(weightsHidden2)
 for i in range(0, len(weightsHidden2)):
     weightRow = weightsHidden2[i]
@@ -53,15 +53,32 @@ for i in range(0, len(weightsHidden2)):
         weightsHidden2Int8[i][j] = to_signed_fixed_point(weight, 8, 4)
 
 np.savetxt('theta_1_int8.csv', weightsHidden2Int8, delimiter=',', fmt='%d')
-
 # Step 3. Multiplication W*X First Hidden Layer
 firstHiddenLayerResult = np.zeros(25)
+# print(weightsHidden1Int8.shape) # (25, 401)
+for i in range(0, weightsHidden1Int8.shape[0]):
+    weights = weightsHidden1Int8[i]
+    sum = 0
+    for j in range(0, len(weights)):
+        weight = weights[j]
+        isNegative = False
+        if weight > 127:
+            isNegative = True
+            weight = 2**8 - weight
+        pixel = int(imagesInt8[j]) << 1
+        tmp = pixel * weight
+        if isNegative and tmp != 0:
+            tmp = 2**16 - tmp
+        tmp = int(tmp) >> 1
+        #print(f"w {weights[j]} p {imagesInt8[j]} res {tmp}")
+        sum = sum + tmp
 
-for i in range(0, len(imagesInt8)):
-    pixel = imagesInt8[i]
-    pass
-    
-
+# Verification
+# px = 2/(2**3) + 2/(2**4) + 2/(2**5) + 2/(2**7)
+# wt = 2/(2**5) + 2/(2**6)
+# app = 2/(2**6) + 2/(2**8) + 2/(2**10) + 2/(2**11) + 2/(2**12)
+# print(f"pixel {px} weight {wt} res {px * wt}")
+# print(f"res {px * wt} == {app}")
 
 # Step 4. Sigmoid operation First Hidden Layer
 sigmoidTmp = np.zeros(25)
