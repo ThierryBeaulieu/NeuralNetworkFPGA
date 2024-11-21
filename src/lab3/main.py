a8f8af0f2e116d407101939c282fe4670c4bdef2
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-20 23:34:00 -0500

    created a conversion for the Weights into CSV and converted Pixels

--- src/lab3/main.py
+++ src/lab3/main.py
@@ -22,25 +22,26 @@ def to_signed_fixed_point(value, bits=8, frac_bits=7):
 
 # Step 1. Converts Pixel Values To Fixed Point Notation [1:7]
 images = np.load("x.npy")
-imagesInt8 = np.zeros_like(images)
+imagesInt8 = np.zeros((5000, 401))
 for i in range(0, len(images)):
     image = images[i]
+    image = np.hstack((1, image))
     for j in range(0, len(image)):
         pixel = image[j]
         imagesInt8[i][j] = to_fixed_point(pixel)
 
 
-
 # Step 2. Converts Weights And Saves Them In CSV [2:6]
 # Hidden Layer 1 [-1.463356, 1.00899]
 weightsHidden1 = np.load("theta_0.npy") # (25, 401)
-weightsHidden1Int8 = np.zeros_like(weightsHidden1)
+weightsHidden1Int8 = np.zeros((26, 401))
 for i in range(0, len(weightsHidden1)):
     weightRow = weightsHidden1[i]
     for j in range(0, len(weightRow)):
         weight = weightRow[j]
         weightsHidden1Int8[i][j] = to_signed_fixed_point(weight, 8, 6)
 
+np.savetxt('theta_0_int8.csv', weightsHidden1Int8, delimiter=',', fmt='%d')
 
 # Hidden Layer 2 [-4.0308, 3.2115848]
 weightsHidden2 = np.load("theta_1.npy") # (25, 401)
@@ -51,8 +52,6 @@ for i in range(0, len(weightsHidden2)):
         weight = weightRow[j]
         weightsHidden2Int8[i][j] = to_signed_fixed_point(weight, 8, 4)
 
+np.savetxt('theta_1_int8.csv', weightsHidden2Int8, delimiter=',', fmt='%d')
 
-# Example usage
-values = [0.5, -0.5, 0.25, -0.25, -1, 1]
-binary_representations = [to_signed_fixed_point(v) for v in values]
-print(binary_representations)
+# Step 3. Make The Multiplication W*X
