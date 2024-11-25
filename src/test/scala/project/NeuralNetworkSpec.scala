2de4ddceb7407f94a4292dc10285660611ef6513
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-25 15:26:00 -0500

    created some basic neural network

---
+++ src/test/scala/project/NeuralNetworkSpec.scala
@@ -0,0 +1,21 @@
+package project
+
+import chisel3._
+import chisel3.simulator.EphemeralSimulator._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+// testOnly project.NeuralNetworkSpec
+
+class NeuralNetworkSpec extends AnyFreeSpec with Matchers {
+  "NeuralNetwork should generate 10 sum as it's output" in {
+    simulate(new NeuralNetwork) { dut =>
+      // Reset the DUT
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(1)
+
+    }
+  }
+}
