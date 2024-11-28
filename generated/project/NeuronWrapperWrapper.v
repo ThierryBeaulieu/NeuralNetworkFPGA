bee45b52ab0cbfa8f0705c064831ed1e0eafe983
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-27 21:48:57 -0500

    neuron wrapper updated and nastanh too

--- generated/project/NeuronWrapperWrapper.v
+++ generated/project/NeuronWrapperWrapper.v
@@ -1,4 +1,4 @@
-module NeuralNetworkWrapper(
+module NeuronWrapperWrapper(
 (* mark_debug = "true" *) input        clock,
 (* mark_debug = "true" *) input        reset,
 (* mark_debug = "true" *) input  [7:0] s_axis_tdata,
@@ -6,14 +6,14 @@ module NeuralNetworkWrapper(
 (* mark_debug = "true" *) input        s_axis_tvalid,
 (* mark_debug = "true" *) input        s_axis_tlast,
 (* mark_debug = "true" *) output       s_axis_tready,
-(* mark_debug = "true" *) output [16:0] m_axis_tdata,
+(* mark_debug = "true" *) output [15:0] m_axis_tdata,
 (* mark_debug = "true" *) output       m_axis_tkeep,
 (* mark_debug = "true" *) output       m_axis_tvalid,
 (* mark_debug = "true" *) output       m_axis_tlast,
 (* mark_debug = "true" *) input        m_axis_tready
 );
 
-NeuralNetwork inst(
+NeuronWrapper inst(
     .clock(clock),
     .reset(!reset),
     .s_axis_tdata(s_axis_tdata),
