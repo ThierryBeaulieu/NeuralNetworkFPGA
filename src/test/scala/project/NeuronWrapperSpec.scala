8ff1aa13096feaec68df205fce96cf4988319f4e
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-30 13:59:14 -0500

    able to display a single result from the new row of results

--- src/test/scala/project/NeuronWrapperSpec.scala
+++ src/test/scala/project/NeuronWrapperSpec.scala
@@ -9,7 +9,7 @@ import org.scalatest.matchers.must.Matchers
 
 class NeuronWrapperSpec extends AnyFreeSpec with Matchers {
 
-  "NeuronWrapperSpec should have accessible weights" in {
+  "NeuronWrapper should be able to make Stanh of 0.5 : 0 < x < 1" in {
     simulate(new NeuronWrapper(8, 128, "weights.csv")) { dut =>
       // Reset the DUT
       dut.reset.poke(true.B)
@@ -18,15 +18,6 @@ class NeuronWrapperSpec extends AnyFreeSpec with Matchers {
 
       dut.weights.length.mustBe(1)
       dut.weights(0).length.mustBe(8)
-    }
-  }
-
-  "NeuronWrapper should be able to make Stanh of 0.5 : 0 < x < 1" in {
-    simulate(new NeuronWrapper(8, 128, "weights.csv")) { dut =>
-      // Reset the DUT
-      dut.reset.poke(true.B)
-      dut.clock.step(1)
-      dut.reset.poke(false.B)
 
       // 0.5 * 0.5 = 0.25
       val imageTest = Seq(255, 255, 255, 255, 255, 255, 255, 255)
@@ -42,7 +33,6 @@ class NeuronWrapperSpec extends AnyFreeSpec with Matchers {
           dut.clock.step(1)
         }
       }
-      dut.clock.step(1)
 
       /*
         val outputB2SValues = Output(Vec(nbData, UInt(1.W)))
@@ -51,21 +41,22 @@ class NeuronWrapperSpec extends AnyFreeSpec with Matchers {
         val outputTreeAdder = Output(SInt((nbData + 1).W))
        */
 
+      // sending
+      dut.masterIO.tready.poke(true.B)
+      dut.clock.step(1)
+
       // handling
       // print(f"B2S[${dut.io.outputB2SValues(1).peek().litValue}]")
       // print(f"B2IS[${dut.io.outputB2ISValues(1).peek().litValue}]")
       // print(f"AND[${dut.io.outputANDValues(1).peek().litValue}]")
       for (_ <- 0 until 10) {
-        print(f"Tree Adder[${dut.io.outputTreeAdder.peek().litValue}]")
+        print(f"[${dut.io.outputTreeAdder.peek().litValue}]")
+        print(f"(${dut.masterIO.tdata.peek().litValue})")
         dut.clock.step(1)
       }
 
-      // sending
-      dut.masterIO.tready.poke(true.B)
-      dut.clock.step(1)
-
-      dut.masterIO.tdata.expect(511.S)
-      dut.masterIO.tlast.expect(true.B)
+      // dut.masterIO.tdata.expect(511.S)
+      // dut.masterIO.tlast.expect(true.B)
 
       // print(f"[${dut.masterIO.tdata.peek().litValue}]")
       // print(f"{${dut.masterIO.tlast.peek().litValue}}")
