ad72cddebf35381ef16c5df41f970d284b2baf79
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-12-02 09:53:14 -0500

    basic implementation of the neural network using types such as int8

--- src/lab3/example.py
+++ src/lab3/example.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(x))
-x = np.load('x.npy')
+    return 1 / (1 + np.exp(-x))
+
+x = np.load('x.npy') # 5000 images
 y = np.load('y.npy')
 
 theta_0 = np.load('theta_0.npy')
@@ -11,9 +12,13 @@ theta_1 = np.load('theta_1.npy')
 idx = 1
 image = x[idx]
 
-xp = np.hstack((1, image))
-res = np.dot(xp, theta_0.T)
-a = sigmoid(res)
+# Calculer sur le FPGA
+xp = np.hstack((1, image)) # Ajouter 1 pour le biais
+a = sigmoid(np.dot(xp,theta_0.T)) # Sortie de la couche 1
+ap = np.hstack((1, a)) # Ajouter 1 pour le biais
+b = sigmoid(np.dot(ap,theta_1.T)) # Sortie de la couche 2
+print("FPGA Output:",b)
 
-print(f"result {res}")
-print(f"sigmoid {a}")
+# Calculer dans le notebook
+pred = b.argmax()
+print("Prediction:", pred, "Label:", y[idx])
---
+++ src/lab3/main.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+debug = False
+
+## Step 0. Define Sigmoid Function And Load Assets
+def sigmoid(x):
+    return 1 / (1 + np.exp(x))
+
+imageMatched = 0
+y = np.load("y.npy")
+theta0 = np.load("theta_0.npy")
+theta1 = np.load("theta_1.npy")
+
+
+for i in range(0, 5000):
+    if i % 100 == 0:
+        print(i)
+    ## Step 1. Represent the Theta_0 Weights In a Fixed Point Representation
+    # [2, 2]
+    if debug:
+        print(f"theta0 min {np.min(theta0)}") # -1.463369
+        print(f"theta0 max {np.max(theta0)}") # 1.0089920
+
+    weightPrecision0 = 2 # 2, 4, 6, 8 ...
+    theta0_UInt8 = np.array([x * 2**weightPrecision0 for x in theta0]).astype(np.int8) # << 2
+
+
+    ## Step 2. Represent the Images In a Fixed Point Representation
+    images = np.load("x.npy")
+
+    # [2, 2]
+    if debug:    
+        print(f"image min {np.min(images)}") # 0.0
+        print(f"image max {np.max(images)}") # 1.0
+
+    image = images[i]
+    xp = np.hstack((1, image))
+
+    imagePrecision = 2 # 2
+    image_UInt8 = np.array([x * 2**imagePrecision for x in xp]).astype(np.int8) # << 2
+
+
+    ## Step 3. We Make the Dot Product Between Image UInt8 and Weight UInt8
+    hiddenLayer0_UInt8 = np.dot(image_UInt8, theta0_UInt8.T) # [4,4]
+
+
+    ## Step 4. Apply the Sigmoid To the Result Hidden Layer 0
+    hiddenLayer0Precision = 4
+    hiddenLayer0_Float = np.array([x/2**hiddenLayer0Precision for x in hiddenLayer0_UInt8]).astype(float) # << 2
+
+    # [2, 6]
+    sigmoid0Precision = 6
+    sig0_Float_tmp = np.array([sigmoid(x) for x in hiddenLayer0_Float]).astype(float) # [0, 1]
+    sig0_Float = np.hstack((1, sig0_Float_tmp))
+    sig0_UInt8 = np.array([x * 2**sigmoid0Precision for x in sig0_Float]).astype(np.int8)
+
+
+    ## Step 5. Represent the Theta_1 Weights In a Fixed Point Representation
+
+    # [3, 1]
+    if debug:
+        print(f"theta1 min {np.min(theta1)}") # -4.030847
+        print(f"theta1 max {np.max(theta1)}") # 3.2115848
+
+    weightPrecision1 = 1
+    theta1_UInt8 = np.array([x * 2**weightPrecision1 for x in theta1]).astype(np.int8) # << 2
+
+
+    ## Step 6. We Make the Dot Product Between Image UInt8 and Weight UInt8
+    hiddenLayer1_UInt8 = np.dot(sig0_UInt8, theta1_UInt8.T) # [5,7]
+
+
+    ## Step 7. Apply the Sigmoid To the Result
+    hiddenLayer1Precision = 7
+    hiddenLayer1_Float = np.array([x/2**hiddenLayer1Precision for x in hiddenLayer1_UInt8]).astype(float) # << 2
+
+    sigmoid1Precision = 6
+    sig1_Float = np.array([sigmoid(x) for x in hiddenLayer1_Float]).astype(float) # [0, 1]
+    sig1_UInt8 = np.array([x * 2**sigmoid1Precision for x in sig1_Float]).astype(np.int8)
+
+    ## Step 8. Compare the result
+    if debug:
+        print("FPGA Output:", sig1_UInt8)
+
+    # Calculer dans le notebook
+    pred = sig1_UInt8.argmax()
+    if pred == y[i]:
+        imageMatched += 1
+
+    if debug:
+        print("Prediction:", pred, "Label:", y[i])
+
+print(f"Success {(imageMatched/5000.0) * 100}")
