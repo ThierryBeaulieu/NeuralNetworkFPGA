d07e09d47e427311083a46fd1b8703205030b149
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-12-02 17:26:35 -0500

    create a class that can handle the dot product

--- src/main/scala/lab3/NeuralNetwork.scala
+++ src/main/scala/lab3/NeuralNetwork.scala
@@ -21,18 +21,21 @@ object Utility {
 }
 
 object State extends ChiselEnum {
-  val receiving, firstHiddenLayer, firstSigmoid, secondHiddenLayer,
-      secondSigmoid, sending =
+  val receiving, firstSigmoid, secondSigmoid, sending =
     Value
 }
 
-object NNHelper {
-  def initializeIO(sAxis: AxiStreamSlaveIf, mAxis: AxiStreamMasterIf) = {
+object NetworkHelper {
+  def initializeIO(
+      sAxis: AxiStreamSlaveIf,
+      mAxis: AxiStreamMasterIf,
+      outputWidth: Int
+  ) = {
     sAxis.tready := RegInit(true.B)
     mAxis.data.tvalid := RegInit(false.B)
     mAxis.data.tlast := RegInit(false.B)
-    mAxis.data.tdata := RegInit(0.S(16.W))
-    mAxis.data.tkeep := RegInit("b11".U)
+    mAxis.data.tdata := RegInit(0.S(outputWidth.W))
+    mAxis.data.tkeep := RegInit("b1".U)
   }
 
   def connectMaster(masterIO: AxiStreamExternalIf, mAxis: AxiStreamMasterIf) = {
@@ -44,34 +47,55 @@ object NNHelper {
   def connectSlave(slaveIO: AxiStreamExternalIf, sAxis: AxiStreamSlaveIf) = {
     slaveIO.suggestName("s_axis").connect(sAxis)
   }
-
 }
 
-class NeuralNetwork extends Module {
+class HiddenLayer0(theta0: Vec[Vec[SInt]], sum: Vec[SInt]) extends Module {
+  var col = 1
+
+  def handlePixel(pixel: SInt) = {
+    for (row <- 0 until 25) {
+      sum(row) := sum(row) + pixel * theta0(row)(col)
+    }
+    if (col == (25 - 1)) {
+      col = 0
+    } else {
+      col = col + 1
+    }
+  }
+}
+
+class NeuralNetwork(inputWidth: Int = 8, outputWidth: Int = 8) extends Module {
   val io = IO(new Bundle {
-    val slaveIO = new AxiStreamExternalIf(8)
-    val masterIO = Flipped(new AxiStreamExternalIf(8))
+    val slaveIO = new AxiStreamExternalIf(inputWidth)
+    val masterIO = Flipped(new AxiStreamExternalIf(inputWidth))
   })
 
-  val sAxis: AxiStreamSlaveIf = Wire(new AxiStreamSlaveIf(8))
-  val mAxis: AxiStreamMasterIf = Wire(new AxiStreamMasterIf(8))
+  val sAxis: AxiStreamSlaveIf = Wire(new AxiStreamSlaveIf(outputWidth))
+  val mAxis: AxiStreamMasterIf = Wire(new AxiStreamMasterIf(outputWidth))
 
-  NNHelper.connectMaster(io.masterIO, mAxis)
-  NNHelper.connectSlave(io.slaveIO, sAxis)
-  NNHelper.initializeIO(sAxis, mAxis)
+  NetworkHelper.connectMaster(io.masterIO, mAxis)
+  NetworkHelper.connectSlave(io.slaveIO, sAxis)
+  NetworkHelper.initializeIO(sAxis, mAxis, outputWidth)
 
   val theta_Int8_csv = Utility.readCSV("lab3/theta0_Int8.csv")
-  val theta0 = RegInit(VecInit.tabulate(25, 401) { (x, y) =>
+  val theta0: Vec[Vec[SInt]] = RegInit(VecInit.tabulate(25, 401) { (x, y) =>
     theta_Int8_csv(x)(y).S(8.W)
   })
 
   val state = RegInit(State.receiving)
+  val hiddenLayer0_result: Vec[SInt] = RegInit(VecInit(Seq.fill(25)(0.S(25.W))))
+  val hiddenLayer0 = Module(new HiddenLayer0(theta0, hiddenLayer0_result))
 
   switch(state) {
-    is(State.receiving) {}
-    is(State.firstHiddenLayer) {}
+    is(State.receiving) {
+      when(sAxis.data.tvalid) {
+        hiddenLayer0.handlePixel(sAxis.data.tdata)
+        when(sAxis.data.tlast) {
+          state := State.firstSigmoid
+        }
+      }
+    }
     is(State.firstSigmoid) {}
-    is(State.secondHiddenLayer) {}
     is(State.secondSigmoid) {}
     is(State.sending) {}
   }
